29ae4be15336100839ad9e75ea23cb96a8c04189
Ahmed175 <https://github.com/Ahmed-175/chat-app-ahemd.git>
2024-05-22 15:27:40 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from './Components/Sidebar/Sidebar'
+import Container from './Components/Container/Container'
+import './App.css';
+const App = () => {
+  return (
+    <div>
+      <Sidebar />
+      <Container/>
+    </div>
+  )
+}
+export default App
